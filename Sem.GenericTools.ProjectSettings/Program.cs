444e97f5d6deb53c66791034c240e2f0ae9e2547
Matzen_cp <SND\Matzen_cp>
2009-09-23 08:57:32 +0000

    added project-property reader to solution

--- Sem.GenericTools.ProjectSettings/Program.cs
+++ Sem.GenericTools.ProjectSettings/Program.cs
@@ -2,6 +2,8 @@
 {
     using System.IO;
     using System.Xml;
+    using System.Reflection;
+    using System;
 
     /// <summary>
     /// This program reads and writes project settings from project files
@@ -12,16 +14,30 @@
     {
         private static readonly string[] Selectors = new []
                 {
-                    @"//cs:Project/cs:PropertyGroup/cs:Configuration",
                     @"//cs:Project/cs:PropertyGroup/cs:RootNamespace",
                     @"//cs:Project/cs:PropertyGroup/cs:AssemblyName",
                     @"//cs:Project/cs:PropertyGroup/cs:TargetFrameworkVersion",
-                    @"//cs:Project/cs:PropertyGroup[@cs:Condition="" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ""]/cs:DebugSymbols",
+                    @"//cs:Project/cs:PropertyGroup[@Condition="" {0} ""]/cs:DebugSymbols",
+                    @"//cs:Project/cs:PropertyGroup[@Condition="" {0} ""]/cs:OutputPath",
+                    @"//cs:Project/cs:PropertyGroup[@Condition="" {0} ""]/cs:DefineConstants",
+                    @"//cs:Project/cs:PropertyGroup[@Condition="" {0} ""]/cs:DebugType",
+                };
+
+        private static readonly string[] ConfigurationConditions = new[]
+                {
+                    @"'$(Configuration)' == ''",
+                    @"'$(Configuration)|$(Platform)' == 'Debug|AnyCPU'",
+                    @"'$(Configuration)|$(Platform)' == 'Release|AnyCPU'",
+                    @"'$(Configuration)|$(Platform)' == 'Debug %28CodeAnalysis%29|AnyCPU'",
                 };
 
         static void Main(string[] args)
         {
-            var rootFolderPath = @"C:\CodePlex\SemSync";
+            var rootFolderPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            if (rootFolderPath.IndexOf(Assembly.GetExecutingAssembly().GetName().Name) > -1)
+            {
+                rootFolderPath = rootFolderPath.Substring(0, rootFolderPath.IndexOf(Assembly.GetExecutingAssembly().GetName().Name)); // @"C:\CodePlex\SemSync";
+            }
 
             using (var outStream = new StreamWriter(Path.Combine(rootFolderPath, "projectsettings.csv")))
             {
@@ -31,13 +47,24 @@
                     projectSettings.Load(projectFile);
                     var namespaceManager = new XmlNamespaceManager(projectSettings.NameTable);
                     namespaceManager.AddNamespace("cs", "http://schemas.microsoft.com/developer/msbuild/2003");
-
-
+                    
                     foreach (var selector in Selectors)
                     {
-                        var value = projectSettings.SelectSingleNode(selector, namespaceManager);
-                        outStream.Write(value != null ? value.InnerXml : string.Empty);
-                        outStream.Write("\t");
+                        if (!selector.Contains("{0}"))
+                        {
+                            var value = projectSettings.SelectSingleNode(selector, namespaceManager);
+                            outStream.Write(value != null ? value.InnerXml : string.Empty);
+                            outStream.Write("\t");
+                        }
+                        else
+                        {
+                            foreach (var config in ConfigurationConditions)
+                            {
+                                var value = projectSettings.SelectSingleNode(string.Format(selector, config), namespaceManager);
+                                outStream.Write(value != null ? value.InnerXml : string.Empty);
+                                outStream.Write("\t");
+                            }
+                        }
                     }
 
                     outStream.WriteLine();
@@ -45,6 +72,9 @@
 
                 outStream.Close();
             }
+
+            Console.WriteLine("press ENTER to write the manipulated properties back");
+            Console.ReadLine();
         }
     }
 }
