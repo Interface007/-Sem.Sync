fb1aeb8f6e4d0d8e71a403219422a4c310894059
Matzen_cp <SND\Matzen_cp>
2009-09-23 06:29:23 +0000

    Starting project to manage cs-project-properties

---
+++ Sem.GenericTools.ProjectSettings/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Sem.GenericTools.ProjectSettings
+{
+    using System.IO;
+    using System.Xml;
+
+    /// <summary>
+    /// This program reads and writes project settings from project files
+    /// to a flat file in tsv format. This was it's easy to compare the 
+    /// settings between different parts of a solution.
+    /// </summary>
+    class Program
+    {
+        private static readonly string[] Selectors = new []
+                {
+                    @"//cs:Project/cs:PropertyGroup/cs:Configuration",
+                    @"//cs:Project/cs:PropertyGroup/cs:RootNamespace",
+                    @"//cs:Project/cs:PropertyGroup/cs:AssemblyName",
+                    @"//cs:Project/cs:PropertyGroup/cs:TargetFrameworkVersion",
+                    @"//cs:Project/cs:PropertyGroup[@cs:Condition="" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ""]/cs:DebugSymbols",
+                };
+
+        static void Main(string[] args)
+        {
+            var rootFolderPath = @"C:\CodePlex\SemSync";
+
+            using (var outStream = new StreamWriter(Path.Combine(rootFolderPath, "projectsettings.csv")))
+            {
+                foreach (var projectFile in Directory.GetFiles(rootFolderPath, "*.csproj", SearchOption.AllDirectories))
+                {
+                    var projectSettings = new XmlDocument();
+                    projectSettings.Load(projectFile);
+                    var namespaceManager = new XmlNamespaceManager(projectSettings.NameTable);
+                    namespaceManager.AddNamespace("cs", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+
+                    foreach (var selector in Selectors)
+                    {
+                        var value = projectSettings.SelectSingleNode(selector, namespaceManager);
+                        outStream.Write(value != null ? value.InnerXml : string.Empty);
+                        outStream.Write("\t");
+                    }
+
+                    outStream.WriteLine();
+                }
+
+                outStream.Close();
+            }
+        }
+    }
+}
