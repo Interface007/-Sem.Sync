9d5ef7f10125b2cd1bd366dcb85962e006f9abe1
Matzen_cp <SND\Matzen_cp>
2009-10-05 10:11:17 +0000

    starting MSAccess connector

---
+++ Sem.Sync.Connector.MsAccess/GenericClient.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GenericClient.cs" company="Sven Erik Matzen">
+//     Copyright (c) Sven Erik Matzen. GNU Library General Public License (LGPL) Version 2.1.
+// </copyright>
+// <author>Sven Erik Matzen</author>
+// <summary>
+//   This class is the client class for handling elements
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Sem.Sync.Connector.MsAccess
+{
+    using System;
+    using System.Collections.Generic;
+
+    using SyncBase;
+    using SyncBase.Attributes;
+
+    /// <summary>
+    /// Class that provides a memory only connector to speed up operations.
+    /// By adding a <see cref="ConnectorDescriptionAttribute"/> with CanRead = false and CanWrite = false
+    /// it's invisible to the client GUI. This attribute is not respected by the engine - only by the GUI.
+    /// </summary>
+    [ConnectorDescription(DisplayName = "MicrosoftAcess-Client",
+        CanReadContacts = true,
+        CanWriteContacts = true)]
+    public class GenericClient : StdClient
+    {
+        /// <summary>
+        /// Gets the user readable name of the client implementation. This name should
+        /// be specific enough to let the user know what element store will be accessed.
+        /// </summary>
+        public override string FriendlyClientName
+        {
+            get
+            {
+                return "Microsoft Access Generic Connector";
+            }
+        }
+
+        /// <summary>
+        /// Abstract read method for full list of elements - this is part of the minimum that needs to be overridden
+        /// </summary>
+        /// <param name="clientFolderName">the information from where inside the source the elements should be read - 
+        /// This does not need to be a real "path", but need to be something that can be expressed as a string</param>
+        /// <param name="result">The list of elements that should get the elements. The elements should be added to
+        /// the list instead of replacing it.</param>
+        /// <returns>The list with the newly added elements</returns>
+        protected override List<StdElement> ReadFullList(string clientFolderName, List<StdElement> result)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Abstract write method for full list of elements - this is part of the minimum that needs to be overridden
+        /// </summary>
+        /// <param name="elements">the list of elements that should be written to the target system.</param>
+        /// <param name="clientFolderName">the information to where inside the source the elements should be written - 
+        /// This does not need to be a real "path", but need to be something that can be expressed as a string</param>
+        /// <param name="skipIfExisting">specifies whether existing elements should be updated or simply left as they are</param>
+        protected override void WriteFullList(List<StdElement> elements, string clientFolderName, bool skipIfExisting)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
